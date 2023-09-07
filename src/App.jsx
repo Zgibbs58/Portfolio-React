0a61821c72aa7b8112b3850a31d2975e3c3c258b
Zgibbs58 <zacharywgibbs@gmail.com>
2023-09-06 21:55:29 -0500

    Initialized a new React app using Vite and integrated Tailwind CSS.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold text-blue-500'>Hello, my name is Zach.</h1>
+    </>
+  )
+}
+
+export default App
